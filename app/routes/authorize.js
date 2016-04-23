38c8d87c0b0bb0bbe6bd95f8e3cd08ecedfa5ad8
sandinosaso <sandinosaso@gmail.com>
2016-04-23 13:23:19 -0300

    Authorize route to spotify api

---
+++ app/routes/authorize.js
@@ -0,0 +1,15 @@
+import Ember from "ember";
+
+export default Ember.Route.extend({
+  // activate: function() {},
+  // deactivate: function() {},
+  // setupController: function(controller, model) {},
+  // renderTemplate: function() {},
+  // beforeModel: function() {},
+  // afterModel: function() {},
+
+  model: function(params) {
+  	  console.log("Authorize spotify params:", params);
+      return ;
+  }
+});
